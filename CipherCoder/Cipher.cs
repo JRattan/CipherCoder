9a162ae949a15e3e25bed90c66ea8fbd32520b8b
JRattan <jonnbeltran23@gmail.com>
2013-09-28 19:55:23 -0700

    first commit

---
+++ CipherCoder/Cipher.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CipherCoder
+{
+    class Cipher
+    {
+        /*
+         * The encryption process works like this:
+         * Using Caesar's cipher, each ASCII code is shifted by 3.
+         * The result yields a set of integers since some letters (such as 'X', 'Y', and 'Z' when shifted will no longer be a letter)
+         * Next each digit of a letter will be converted to its own letter. So an integer 123 for example would become '1', '2', '3' which whould
+         * then be converted as 'A', 'C', 'E' or 'B', 'D', 'F' or any combination of those.
+         * Each integer would be separated by a 'U', 'V', 'W', 'X', 'Y', or 'Z'
+         * Now that the set is now composed of letters, the program will be making use of Vignere cipher using the key entered.
+         * Finally the resulting string will be reversed
+         */
+        public static string Encrypt(string input, string key)
+        {
+            if (!IsAllLetters(key))
+                throw new Exception("Key must consist of only letters!");
+            return ReverseString(Vignere(EnSecondPass(EnFirstPass(input)), key));
+        }
+
+        /*
+         * The decryption process works like this:
+         * First the string will be reversed back to normal
+         * Vignere cipher will be implemented then 
+         * The letters will then be converted back to numbers, if it's from 'U' to 'Z' then it is converted to 'X'
+         * The numbers will then be combined to its original integer format. The program loops until it reaches an 'X'
+         * Finally Caesar's cipher will be used and will be shifted by 3.
+         * 
+         */
+        public static string Decrypt(string input, string key)
+        {
+            if (!IsAllLetters(input))
+                throw new Exception("When decrypting, Input must only be composed of letters!");
+            if (!IsAllLetters(key))
+                throw new Exception("Key must consist of only letters!");
+
+            return DeSecondPass(DeFirstPass(Vignere(ReverseString(input).ToUpper(), key, -1)));
+        }
+
+        private static bool IsAllLetters(string message)
+        {
+            foreach (char i in message)
+            {
+                if (i == '\n') continue;
+
+                int j = (int)i;
+                if (i < 'A' || (i > 'Z' && i < 'a') || i > 'z')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static string ReverseString(string text)
+        {
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        private static string EnFirstPass(string message)
+        {
+            string newMessage = "";
+
+            foreach (char i in message)
+            {
+                newMessage += String.Format("{0}X", i + 3);
+            }
+
+            return newMessage;
+        }
+
+        private static string EnSecondPass(string message)
+        {
+            string newMessage = "";
+            Random rand = new Random();
+
+            foreach (char i in message)
+            {
+                int k = 0;
+                if (i != 'X')
+                {
+                    k = (int)Char.GetNumericValue(i);
+
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (k == j) newMessage += (char)(rand.Next('A' + (2 * j), 'C' + (2 * j)));
+                    }
+                }
+                else newMessage += (char)(rand.Next(85, 91));
+            }
+
+            return newMessage;
+        }
+
+        private static string DeFirstPass(string message)
+        {
+            string newMessage = "";
+
+            foreach (char i in message)
+            {
+                if (i == '\n') continue;
+
+                for (int j = 0; j < 10; j++)
+                {
+                    if ((int)i == ('A' + (2 * j)) || (int)i == ('B' + (2 * j))) newMessage += String.Format("{0}", j);
+                }
+                if (i >= 'U' && i <= 'Z') newMessage += 'X';
+            }
+
+            return newMessage;
+        }
+
+        private static string DeSecondPass(string message)
+        {
+            string newMessage = "";
+            string digit = "";
+
+            foreach (char i in message)
+            {
+                if (i == '\n') continue;
+
+                if (i == 'X')
+                {
+                    newMessage += String.Format("{0}", (char)(Int32.Parse(digit) - 3));
+                    digit = "";
+                }
+                else digit += i;
+            }
+
+            return newMessage;
+        }
+
+        private static string Vignere(string message, string keyword, int encrypt = 1)
+        {
+            string newMessage = "";
+
+            message = message.ToUpper();
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == '\n') continue;
+
+                int vt = (int)message[i];
+                int vk = (int)keyword[i % keyword.Length];
+
+                int vr = vt + (vk * encrypt);
+                if (vr < 0) vr += 26;
+                newMessage += (char)((vr % 26) + 'A');
+            }
+
+            return newMessage;
+        }
+    }
+}
---
+++ CipherCoder/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace CipherCoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Cipher cipher = new Cipher();
+     
+        public MainWindow()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        public void Reset()
+        {
+            inputText.Text = "";
+            outputText.Text = "";
+            keyText.Text = "CIPHER";
+            //CipherAssign();
+        }
+
+        /*
+        public void CipherAssign()
+        {
+            cipher.Input = inputText.Text.ToString();
+            cipher.Key = keyText.Text.ToString().Replace(" ", String.Empty);
+        }
+        */
+
+        /*
+        public void CipherUpdate()
+        {
+            if (String.IsNullOrWhiteSpace(outputText.Text))
+                MessageBox.Show("There was nothing to encrypt/decrypt.", "Empty String");
+        }
+        */
+
+        public void UseOutput()
+        {
+            inputText.Text = outputText.Text;
+        }
+
+        private void encryptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (keyText.Text.ToString().Length == 0)
+                    throw new Exception("Cipher key is empty!");
+                keyText.Text = keyText.Text.ToString().ToUpper().Replace(" ", String.Empty);
+                outputText.Text = Cipher.Encrypt(inputText.Text.ToString(), keyText.Text.ToString().Replace(" ", String.Empty));
+
+                if (String.IsNullOrWhiteSpace(outputText.Text.ToString()))
+                    MessageBox.Show("There was nothing to encrypt.", "Empty String");
+            }
+            catch (ArgumentException ex)
+            {
+                outputText.Text = ex.Message;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void switchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UseOutput();
+        }
+
+        private void decryptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (keyText.Text.ToString().Length == 0)
+                    throw new Exception("Cipher key is empty!");
+                keyText.Text = keyText.Text.ToString().ToUpper().Replace(" ", String.Empty);
+                outputText.Text = Cipher.Decrypt(inputText.Text.ToString(), keyText.Text.ToString().Replace(" ", String.Empty));
+
+                if (String.IsNullOrWhiteSpace(outputText.Text.ToString()))
+                    MessageBox.Show("There was nothing to decrypt.", "Empty String");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            About aboutWindow = new About();
+            aboutWindow.ShowDialog();
+        }
+
+        private void exportFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(outputText.Text.ToString()))
+                MessageBox.Show("Cannot save. Ciphered output text is empty.", "No Text to Save");
+            else
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.DefaultExt = "txt";
+                saveDialog.AddExtension = true;
+                saveDialog.Filter = "Text Document (*.txt)|*.txt|All files(*.*)|*.*";
+                saveDialog.FileName = "CipherCode";
+                saveDialog.OverwritePrompt = true;
+                saveDialog.Title = "Cipher Coder";
+                saveDialog.ValidateNames = true;
+
+                if (saveDialog.ShowDialog().Value)
+                {
+                    using (StreamWriter writer = new StreamWriter(saveDialog.FileName))
+                        writer.WriteLine(outputText.Text.ToString());
+
+                    MessageBox.Show("Ciphered code saved!", "Success");
+                }
+            }
+        }
+
+        private void importFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Text Document (*.txt)|*.txt|All files(*.*)|*.*";
+            openDialog.Title = "Cipher Coder";
+
+            if (openDialog.ShowDialog().Value)
+            {
+                using (StreamReader reader = new StreamReader(openDialog.FileName))
+                {
+                    inputText.Text = "";
+                    while (reader.Peek() >= 0)
+                    {
+                        inputText.Text += reader.ReadLine();
+                        inputText.Text += "\n";
+                    }
+                }
+            }
+        }
+
+        private void clearText_Click(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+
+        private void copyAll_Click(object sender, RoutedEventArgs e)
+        {
+            outputText.SelectAll();
+            outputText.Copy();
+        }
+
+        //Temporary method since XAML one isn't working....
+        private void textWrap_Click(object sender, RoutedEventArgs e)
+        {
+            if (textWrap.IsChecked == true)
+            {
+                inputText.TextWrapping = TextWrapping.Wrap;
+                outputText.TextWrapping = TextWrapping.Wrap;
+            }
+            else
+            {
+                inputText.TextWrapping = TextWrapping.NoWrap;
+                outputText.TextWrapping = TextWrapping.NoWrap;
+            }
+        }
+    }
+}
